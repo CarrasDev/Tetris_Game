4d6d02312a4ea2f941d8a9269e78663e6fce75a4
CarrasDev <carrascoluque@gmail.com>
2025-07-31 21:08:05 +0200

    Reformulation of class Piece

--- game/piece.py
+++ game/piece.py
@@ -4,21 +4,21 @@
 # Asignar colores
 
 import random
+
 class Piece:
-    def __init__(self, shape, color):
-        self.shape = shape  # Forma de la pieza (lista de coordenadas)
+    def __init__(self, x, y, shape, color):
+        self.x = x  # Posición horizontal en el tablero
+        self.y = y  # Posición vertical en el tablero
+        self.shape = shape  # Forma de la pieza
         self.color = color  # Color de la pieza
-        self.position = (0, 0)  # Posición inicial en el tablero
+        self.rotation = 0   # Estado de rotación de la pieza
+
+    def get_current_shape(self):
+        return self.shape[self.rotation]    # Obtener la forma actual de la pieza según su rotación
 
     def rotate(self):
-        """Rota la pieza 90 grados en sentido horario."""
-        self.shape = [(-y, x) for x, y in self.shape]
+        self.rotation = (self.rotation + 1) % len(self.shape) # Rotar la pieza de forma cíclica
 
-    def move(self, dx, dy):
-        """Mueve la pieza a una nueva posición."""
-        self.position = (self.position[0] + dx, self.position[1] + dy)
 
-    def get_coordinates(self):
-        """Devuelve las coordenadas actuales de la pieza en el tablero."""
-        return [(x + self.position[0], y + self.position[1]) for x, y in self.shape]
     
+    # TODO: Proseguir con la implementación de las piezas
