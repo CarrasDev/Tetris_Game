ed2e31ce29c3c3b2db94160570e120b708689f06
CarrasDev <carrascoluque@gmail.com>
2025-08-03 01:01:20 +0200

    In Game Class implements get_random_piece()

---
+++ game/game.py
@@ -0,0 +1,22 @@
+import random
+from.board import Board
+from.piece import Piece
+from.shapes import SHAPES, COLORS
+
+class Game:
+    def __init__(self, width=10, height=20):
+        self.board = Board(width, height)
+        self.current_piece = None
+        self.next_piece = self.get_random_piece()
+        self.score = 0
+        self.game_over = False
+
+    def get_random_piece(self):
+        shape_key = random.choice(list(SHAPES.keys()))
+        shape = SHAPES[shape_key]
+        color = COLORS[shape_key]
+        x = (self.board.width - len(shape[0][0])) // 2
+        y = 0
+        return Piece(x, y, shape, color)
+    
+    
