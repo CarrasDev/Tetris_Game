f9e2ae95a2920126f8e317669ba9ff8559724219
CarrasDev <carrascoluque@gmail.com>
2025-08-03 01:16:15 +0200

    In Game Class: Implements metods rotate_piece(), is_valid_position() & move_piece()

--- game/game.py
+++ game/game.py
@@ -6,7 +6,7 @@ from.shapes import SHAPES, COLORS
 class Game:
     def __init__(self, width=10, height=20):
         self.board = Board(width, height)
-        self.current_piece = None
+        self.current_piece = self.get_random_piece()
         self.next_piece = self.get_random_piece()
         self.score = 0
         self.game_over = False
@@ -18,5 +18,26 @@ class Game:
         x = (self.board.width - len(shape[0][0])) // 2
         y = 0
         return Piece(x, y, shape, color)
+    
+    def rotate_piece(self):
+        self.current_piece.rotate()
+        if not self.is_valid_position():
+            self.current_piece.rotate_left()
+    
+    def is_valid_position(self):
+        for cell in self.current_piece.get_cells():
+            x, y = cell
+            if x < 0 or x >= self.board.width or y < 0 or y >= self.board.height:
+                return False
+            if self.board.grid[y][x] != (0, 0, 0):
+                return False
+        return True
+    
+    def move_piece(self, dx, dy):
+        self.current_piece.x += dx
+        self.current_piece.y += dy
+        if not self.is_valid_position():
+            self.current_piece.x -= dx
+            self.current_piece.y -= dy
     
     
